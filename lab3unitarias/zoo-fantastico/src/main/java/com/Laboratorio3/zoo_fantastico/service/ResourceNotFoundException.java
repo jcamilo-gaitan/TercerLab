9f198019aa68247235d099d8616340cb9e938326
Julian <jcamilo-gaitan@javeriana.edu.co>
2025-11-17 21:10:35 -0500

    Test passed

--- lab3unitarias/zoo-fantastico/src/main/java/com/Laboratorio3/zoo_fantastico/service/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.Laboratorio3.zoo_fantastico.service;
-import java.lang.RuntimeException;
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
